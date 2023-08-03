fce71d54f6e2898c7f94bae2a4724521066b66b4
Budarpu <Budarpumadhu22@gmail.com>
2023-08-03 16:40:54 +0530

    fifth commit

--- wdio.conf.js
+++ wdio.conf.js
@@ -67,14 +67,14 @@ export const config = {
         browserName: 'chrome',
         maxInstances: 10
     },
-    {
-        browserName: 'firefox',
-        maxInstances: 1
-    },
-    {
-        browserName: 'MicrosoftEdge',
-        maxInstances: 1
-    }
+    // {
+    //     browserName: 'firefox',
+    //     maxInstances: 1
+    // },
+    // {
+    //     browserName: 'MicrosoftEdge',
+    //     maxInstances: 1
+    // }
 ],
 
     //
@@ -124,7 +124,7 @@ export const config = {
     // Services take over a specific job you don't want to take care of. They enhance
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
-    services: ['selenium-standalone'],//chromedriver selenium-standalone
+    services: ['chromedriver'],//chromedriver selenium-standalone
 
     // Framework you want to run your specs with.
     // The following are supported: Mocha, Jasmine, and Cucumber
